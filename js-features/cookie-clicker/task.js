6edc2eda3c4e57e90f4e02e5e3c86d79af3e60d9
Анастасия Лежнева <alzhnev@gmail.com>
2022-07-23 18:23:20 +0500

    fix second task

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,19 +1,15 @@
 'use strict';
 
-const cookie = document.getElementById('cookie');
 const counter = document.getElementById('clicker__counter');
-const speedometer = document.getElementById('clicker__speed');
-let count = 0;
-let firstClick;
-let secondClick;
+const cookie = document.getElementById('cookie');
 
-function resizeAndCount() {
-   secondClick === undefined ? secondClick = 0 : secondClick = firstClick;
-   firstClick = new Date();
-   cookie.width === 200 ? cookie.width = 220 : cookie.width = 200;
-   speedometer.textContent = String((1 / (firstClick - secondClick) * 1000).toFixed(2));
-   count++;
-   counter.textContent = String(count);
-}
 
-cookie.onclick = resizeAndCount;
+cookie.onclick = function () {
+   counter.innerText++;
+   const num = counter.innerText;
+   if (num % 2 === 0) {
+      cookie.width /= 2;
+   } else {
+      cookie.width *= 2;
+   }
+};
