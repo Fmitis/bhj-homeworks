21a990d395cfd7f4918374f6d3e7efd8b5400dc8
Анастасия Лежнева <alzhnev@gmail.com>
2022-07-23 18:50:02 +0500

    third

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -1,19 +1,19 @@
 class Autocomplete {
-  constructor( container ) {
+  constructor(container) {
     this.container = container;
-    this.input = container.querySelector( '.autocomplete__input' );
-    this.searchInput = container.querySelector( '.autocomplete__search' );
-    this.list = container.querySelector( '.autocomplete__list' );
-    this.valueContainer = container.querySelector( '.autocomplete__value' );
-    this.valueElement = container.querySelector( '.autocomplete__text-content' );
+    this.input = container.querySelector('.autocomplete__input');
+    this.searchInput = container.querySelector('.autocomplete__search');
+    this.list = container.querySelector('.autocomplete__list');
+    this.valueContainer = container.querySelector('.autocomplete__value');
+    this.valueElement = container.querySelector('.autocomplete__text-content');
 
     this.registerEvents();
   }
 
   registerEvents() {
-    this.valueContainer.addEventListener( 'click', e => {
-      this.searchInput.classList.add( 'autocomplete__search_active' );
-      this.list.classList.add( 'autocomplete__list_active' );
+    this.valueContainer.addEventListener('click', e => {
+      this.searchInput.classList.add('autocomplete__search_active');
+      this.list.classList.add('autocomplete__list_active');
       this.searchInput.value = this.valueElement.textContent.trim();
       this.searchInput.focus();
 
@@ -21,11 +21,11 @@ class Autocomplete {
     });
 
 
-    this.searchInput.addEventListener( 'input', e => this.onSearch());
+    this.searchInput.addEventListener('input', e => this.onSearch());
 
-    this.list.addEventListener( 'click', e => {
+    this.list.addEventListener('click', e => {
       const { target } = e;
-      if ( !target.matches( '.autocomplete__item' )) {
+      if (!target.matches('.autocomplete__item')) {
         return;
       }
 
@@ -40,25 +40,25 @@ class Autocomplete {
     });
   }
 
-  onSelect( item ) {
+  onSelect(item) {
     this.input.selectedIndex = item.index;
     this.valueElement.textContent = item.text;
 
-    this.searchInput.classList.remove( 'autocomplete__search_active' );
-    this.list.classList.remove( 'autocomplete__list_active' );
+    this.searchInput.classList.remove('autocomplete__search_active');
+    this.list.classList.remove('autocomplete__list_active');
   }
 
   onSearch() {
-    const matches = this.getMatches( this.searchInput.value );
+    const matches = this.getMatches(this.searchInput.value);
 
-    this.renderMatches( matches );
+    this.renderMatches(matches);
   }
 
-  renderMatches( matches ) {
-    const html = matches.map( item => `
-    	<li>
+  renderMatches(matches) {
+    const html = matches.map(item => `
+    <li>
         <span class="autocomplete__item"
-        	data-index="${item.index}"
+        data-index="${item.index}"
           data-id="${item.value}"
         >${item.text}</span>
       </li>
@@ -67,27 +67,22 @@ class Autocomplete {
     this.list.innerHTML = html.join('');
   }
 
-  getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
+  getMatches(text) {
+    const arrText = this.input.options;
+    const result = [];
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
+    for (let i = 0; i < arrText.length; i++) {
+      let elem = arrText[i];
+
+      if (elem.innerHTML.includes(text)) {
+        result.push({
+          text: `${elem.innerHTML}`,
+          value: `${elem.value}`,
+        });
       }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    }
+    return result;
   }
 }
 
-new Autocomplete( document.querySelector( '.autocomplete' ));
+new Autocomplete(document.querySelector('.autocomplete'));
